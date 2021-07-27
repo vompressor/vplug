826107c8178c79fb30a41114c37f391f4ee45267
vompressor <vompressor@gmail.com>
2021-07-27 13:28:51 +0000

    add get plugin info

--- loader/loader.go
+++ loader/loader.go
@@ -7,25 +7,37 @@ import (
 	"github.com/vompressor/vplug/plugin"
 )
 
-func Load(plugPath string, sym string) (*plugin.NTFuncs, error) {
+func Load(plugPath string, sym string) (*plugin.NTFuncs, *plugin.PluginInfo, error) {
 	p, err := plg.Open(plugPath)
 	if err != nil {
-		return nil, err
-	}
-
-	s, err := p.Lookup(sym)
-
-	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 	var d *plugin.NTFuncs
+	var i *plugin.PluginInfo
 	var ok bool
-	d, ok = s.(*plugin.NTFuncs)
-	if !ok {
-		return nil, errors.New("load symbolic error")
+
+	s, err := p.Lookup("PluginInfo")
+	if err != nil {
+		return nil, nil, err
 	}
 
-	return d, nil
+	i, ok = s.(*plugin.PluginInfo)
+	if !ok {
+		return nil, nil, errors.New("plugin info casting error")
+	}
+
+	s, err = p.Lookup(sym)
+
+	if err != nil {
+		return nil, i, err
+	}
+
+	d, ok = s.(*plugin.NTFuncs)
+	if !ok {
+		return nil, i, errors.New("type casting error")
+	}
+
+	return d, i, nil
 }
 
 // TODO::
